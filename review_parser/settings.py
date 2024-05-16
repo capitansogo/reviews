15d8c4c66b61ccc6ed9cda8a6048817ee28b19a9
aleksandrkongurov <alexfake95@gmail.com>
2024-05-17 01:59:06 +0300

    custom design

--- review_parser/settings.py
+++ review_parser/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://5068-5-167-37-93.ngrok-free.app', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app', 'https://*.127.0.0.1']
 
 
 # Application definition
